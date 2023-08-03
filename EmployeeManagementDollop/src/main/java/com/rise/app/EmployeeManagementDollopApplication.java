f057288647ea7c0e8c518b26cfb3e9964b668d39
harry <rohansharma39033@gmail.com>
2023-08-03 12:51:55 +0530

    Comment

---
+++ EmployeeManagementDollop/src/main/java/com/rise/app/EmployeeManagementDollopApplication.java
@@ -0,0 +1,13 @@
+package com.rise.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementDollopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementDollopApplication.class, args);
+	}
+
+}
---
+++ EmployeeManagementDollop/src/test/java/com/rise/app/EmployeeManagementDollopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rise.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementDollopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
